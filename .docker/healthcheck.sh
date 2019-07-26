880119431ccc68092c2f21e4eff49a4f469b7d15
NoName <contact@cedricduffournet.com>
2019-07-26 12:53:09 +0200

    Fix error in healthcheck

--- .docker/healthcheck.sh
+++ .docker/healthcheck.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-while [ "$(docker inspect -f '{{.State.Health.Status}}' symfony-oauth_db)" != "healthy" ];
+while [ "$(docker inspect -f '{{.State.Health.Status}}' symfony-demo_db)" != "healthy" ];
 do
     sleep 1
 done
