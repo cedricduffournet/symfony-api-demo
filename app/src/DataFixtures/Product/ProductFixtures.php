bcb2b9b0ca97fe2b0a7aab1a473ac3fe4c9647d6
NoName <contact@cedricduffournet.com>
2019-07-31 12:22:15 +0200

    improve product fixtures

--- app/src/DataFixtures/Product/ProductFixtures.php
+++ app/src/DataFixtures/Product/ProductFixtures.php
@@ -3,47 +3,24 @@
 namespace App\DataFixtures\Product;
 
 use App\DataFixtures\CustomNativeLoader;
-use App\Service\ProductServiceInterface;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
-use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class ProductFixtures extends Fixture implements FixtureGroupInterface, DependentFixtureInterface
+class ProductFixtures extends Fixture implements FixtureGroupInterface
 {
-    /**
-     * @var ProductService
-     */
-    private $productService;
-
-    public function __construct(ProductServiceInterface $productService)
-    {
-        $this->productService = $productService;
-    }
-
     public function load(ObjectManager $manager)
-    {
-        $this->loadProducts();
-    }
-
-    private function loadProducts(): void
     {
         $loader = new CustomNativeLoader();
         $objectSet = $loader->loadFile(__DIR__.'/Data/products.yaml')->getObjects();
         foreach ($objectSet as $object) {
-            $this->productService->updateProduct($object);
+            $manager->persist($object);
+            $manager->flush();
         }
     }
 
     public static function getGroups(): array
     {
         return ['product'];
-    }
-
-    public function getDependencies(): array
-    {
-        return [
-            ProductCategoryFixtures::class,
-        ];
     }
 }
